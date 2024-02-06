ad51a2e14db1ffa94ae3ad3c220a27722d552b92
RaulBirlem <raul.birlem@gmail.com>
2024-02-06 15:31:17 -0300

    font

--- nlw-expert-notes/src/app.tsx
+++ nlw-expert-notes/src/app.tsx
@@ -1,6 +1,10 @@
-
+import React from "react"
 
 export function App() {
-  return <h1>hello world</h1>
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
 }
 
